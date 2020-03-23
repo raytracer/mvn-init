fd633fc5ebf0e7970bfa31d94599d258aca0cd3b
Christoph Müller <mueller@olyro.de>
2020-03-23 13:09:14 +0100

    better error handling to avoid most .unwrap() calls

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ extern crate tinytemplate;
 
 use serde_json::value::Value;
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
@@ -19,13 +20,19 @@ struct Context {
     java_version: String,
 }
 
-fn main() {
+fn main() -> std::result::Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 4 {
         println!("3 arguments (name, package, java version) are required");
-        return;
+        return Ok(());
     }
+
+    let context = Context {
+        name: args.get(1).unwrap().to_string(),
+        package: args.get(2).unwrap().to_string(),
+        java_version: args.get(3).unwrap().to_string(),
+    };
 
     let pomxml = include_str!("../pom.xml.template");
     let source = include_str!("../Main.java.template");
@@ -38,17 +45,11 @@ fn main() {
 
         return Ok(());
     });
-    tt.add_template("pomxml", pomxml).unwrap();
-    tt.add_template("source", source).unwrap();
+    tt.add_template("pomxml", pomxml)?;
+    tt.add_template("source", source)?;
 
-    let context = Context {
-        name: args.get(1).unwrap().to_string(),
-        package: args.get(2).unwrap().to_string(),
-        java_version: args.get(3).unwrap().to_string(),
-    };
-
-    let rendered_pom = tt.render("pomxml", &context).unwrap();
-    let rendered_source = tt.render("source", &context).unwrap();
+    let rendered_pom = tt.render("pomxml", &context)?;
+    let rendered_source = tt.render("source", &context)?;
     let base = Path::new(&context.name);
 
     let src_path = base
@@ -56,15 +57,17 @@ fn main() {
         .join(context.package.replace(".", "/"))
         .join(context.name.to_lowercase());
 
-    fs::create_dir(base).unwrap();
+    fs::create_dir(base)?;
 
-    fs::create_dir_all(src_path.clone()).unwrap();
+    fs::create_dir_all(src_path.clone())?;
 
-    let mut f_pom = File::create(base.join("pom.xml")).unwrap();
-    f_pom.write_all(rendered_pom.as_bytes()).unwrap();
-    f_pom.sync_data().unwrap();
+    let mut f_pom = File::create(base.join("pom.xml"))?;
+    f_pom.write_all(rendered_pom.as_bytes())?;
+    f_pom.sync_data()?;
 
-    let mut f_source = File::create(src_path.join("Main.java")).unwrap();
-    f_source.write_all(rendered_source.as_bytes()).unwrap();
-    f_source.sync_data().unwrap();
+    let mut f_source = File::create(src_path.join("Main.java"))?;
+    f_source.write_all(rendered_source.as_bytes())?;
+    f_source.sync_data()?;
+
+    return Ok(());
 }
